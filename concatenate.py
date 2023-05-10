c037d0fdd638027e6b338dfe7dbb03f0ddac3a50
Daniel Ben-David <DANBEN2@niles-hs.k12.il.us>
2023-05-10 08:27:50 -0500

    githubRepo

---
+++ concatenate.py
@@ -0,0 +1,27 @@
+firstNum = "2"
+secondNum = "2"
+# firstNum = "15"
+# secondNum = "
+if len(firstNum) < len(secondNum):
+    numZeros = len(secondNum) - len(firstNum)
+    for x in range(numZeros):
+        firstNum = "0" + firstNum
+
+if len(secondNum) < len(firstNum):
+    numZeros = len(firstNum) - len(secondNum)
+    for x in range(numZeros):
+        secondNum = "0" + secondNum
+
+def add(a,b):
+    carry = 0
+    answer = ""
+    for x in reversed(range(len(a))):
+        temp = int(a[x]) + int(b[x]) + carry
+        answer = str(temp % 10) + answer 
+        carry = 0
+        if temp > 9:
+            carry = 1
+    return answer
+
+
+print(add(firstNum,secondNum))
